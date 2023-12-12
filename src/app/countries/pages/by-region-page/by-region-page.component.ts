fd120c4264e6a6b5044f72909973afe264b041c6
dotJManuel <dotJManuel@outlook.com>
2023-12-11 22:45:56 -0600

    se modifico busqueda por region

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountryService } from '../../services/countries.service';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -11,13 +13,20 @@ import { CountryService } from '../../services/countries.service';
 export class ByRegionPageComponent {
 
   public countries: Country[] = [];
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
+  public isLoading: boolean = false;
 
   constructor( private countryService: CountryService ) {}
 
-  searchByRegion( term: string ):void {
-    this.countryService.searchRegion( term )
+  searchByRegion( region: Region ):void {
+    this.isLoading = true;
+    this.selectedRegion = region;
+
+    this.countryService.searchRegion( region )
       .subscribe( countries => {
         this.countries = countries;
+        this.isLoading = false;
       });
   }
 
